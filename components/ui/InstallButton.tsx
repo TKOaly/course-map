f508f29b1f145ba49c9a038bab6fe09b21a05a73
Joona Kauppila <56773501+Jokauppi@users.noreply.github.com>
2024-08-23 08:07:17 +0300

    Add install button functionality, new sceenshots

--- components/ui/InstallButton.tsx
+++ components/ui/InstallButton.tsx
@@ -4,17 +4,41 @@ import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { ArrowDownToLine } from 'lucide-react'
 import { useSearchParams } from 'next/navigation'
+import { useCallback, useEffect, useState } from 'react'
 
 type OwnProps = {
     includeText?: boolean
     className?: string
 }
 
+interface BeforeInstallPromptEvent extends Event {
+    readonly platforms: string[]
+    readonly userChoice: Promise<{
+        outcome: 'accepted' | 'dismissed'
+        platform: string
+    }>
+    prompt(): Promise<void>
+}
+
 export function InstallButton({ includeText = false, className }: OwnProps) {
     const searchParams = useSearchParams()
+    const [installPrompt, setInstallPrompt] =
+        useState<BeforeInstallPromptEvent | null>(null)
+
+    const onInstallReady = useCallback((e: Event) => {
+        e.preventDefault()
+        setInstallPrompt(e as BeforeInstallPromptEvent)
+    }, [])
+
+    useEffect(() => {
+        window.addEventListener('beforeinstallprompt', onInstallReady)
+        return () =>
+            window.removeEventListener('beforeinstallprompt', onInstallReady)
+    }, [onInstallReady])
+
     if (searchParams.get('source') === 'pwa') return null
 
-    return (
+    return installPrompt ? (
         <Button
             variant="outline"
             size="icon"
@@ -22,9 +46,12 @@ export function InstallButton({ includeText = false, className }: OwnProps) {
                 'flex w-fit items-center justify-between gap-2 border-border px-3',
                 className
             )}
+            onClick={async () => {
+                await installPrompt.prompt()
+            }}
         >
             <ArrowDownToLine className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100" />
-            {includeText && <p>Install App</p>}
+            {includeText && <p>Asenna</p>}
         </Button>
-    )
+    ) : null
 }

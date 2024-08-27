b62ef160411c626ad43fffdfd8e4202e5ee17c08
Joona Kauppila <56773501+Jokauppi@users.noreply.github.com>
2024-08-28 00:17:42 +0300

    Use elk for layouting

--- components/flow/CourseFlow.tsx
+++ components/flow/CourseFlow.tsx
@@ -2,60 +2,76 @@
 
 import { selectedCourseAtom } from '@/lib/state'
 import { useBreakpoint } from '@/lib/tailwind'
-import dagre from '@dagrejs/dagre'
 import { Background, Position, ReactFlow, useReactFlow } from '@xyflow/react'
 import { useSetAtom } from 'jotai'
 import { CourseNode } from './CourseNode'
 
-import { type CourseEdgeType } from '@/lib/edges'
-import { type CourseNodeType } from '@/lib/nodes'
 import '@xyflow/react/dist/style.css'
 import { useEffect, useState } from 'react'
 import { CourseEdge } from './CourseEdge'
 import { type CourseNodeData } from './CourseLoader'
 
-const nodeWidth = 350
-const nodeHeight = 60
+import { type CourseEdgeType } from '@/lib/edges'
+import { type CourseNodeType } from '@/lib/nodes'
+import ELK from 'elkjs'
 
-const getLayoutedElements = (
+const nodeWidth = 350
+const nodeHeight = 80
+
+const elk = new ELK()
+
+const layoutOptions = {
+    'elk.algorithm': 'layered',
+    'elk.direction': 'RIGHT',
+    separateConnectedComponents: 'false',
+    'elk.layered.nodePlacement.bk.fixedAlignment': 'BALANCED',
+    'elk.layered.crossingMinimization.forceNodeModelOrder': 'false',
+    'elk.layered.crossingMinimization.semiInteractive': 'true',
+}
+
+const getLayoutedNodes = async (
     nodes: CourseNodeType[],
     edges: CourseEdgeType[]
 ) => {
-    const dagreGraph = new dagre.graphlib.Graph()
-    dagreGraph.setGraph({ rankdir: 'LR' })
-    dagreGraph.setDefaultEdgeLabel(() => ({}))
+    const graph = {
+        id: 'root',
+        layoutOptions,
+        children: nodes.map((node) => {
+            return {
+                id: node.id,
+                width: nodeWidth,
+                height: nodeHeight,
+                targetPosition: Position.Left,
+                sourcePosition: Position.Right,
+                layoutOptions: {
+                    'portAlignment.default': 'CENTER',
+                    portConstraints: 'FIXED_SIDE',
+                },
+            }
+        }),
+        edges: edges.map((e) => ({
+            id: e.id,
+            sources: [e.source],
+            targets: [e.target],
+        })),
+    }
 
-    nodes.forEach((node) => {
-        dagreGraph.setNode(node.id, {
-            width: nodeWidth,
-            height: nodeHeight,
-        })
-    })
+    const layoutedGraph = await elk.layout(graph)
 
-    edges.forEach((edge) => {
-        dagreGraph.setEdge(edge.source, edge.target)
-    })
-
-    dagre.layout(dagreGraph)
-
-    const newNodes = nodes.map((node) => {
-        const nodeWithPosition = dagreGraph.node(node.id)
-        const newNode = {
+    const layoutedNodes = nodes.map((node) => {
+        const layoutedNode = layoutedGraph.children?.find(
+            (lgNode) => lgNode.id === node.id
+        )
+        return {
             ...node,
-            targetPosition: Position.Left,
-            sourcePosition: Position.Right,
-            // We are shifting the dagre node position (anchor=center center) to the top left
-            // so it matches the React Flow node anchor point (top left).
             position: {
-                x: nodeWithPosition.x - nodeWidth / 2,
-                y: nodeWithPosition.y - nodeHeight / 2,
+                x: layoutedNode?.x ?? 0,
+                y: layoutedNode?.y ?? 0,
             },
         }
-
-        return newNode
     })
 
-    return { nodes: newNodes, edges }
+    return layoutedNodes
 }
 
 const nodeTypes = {
@@ -73,28 +89,31 @@ export const CourseFlow = ({
 }) => {
     const selectCourse = useSetAtom(selectedCourseAtom)
     const isMobile = !useBreakpoint('md')
-    const reactFlow = useReactFlow()
+    const { fitView, getViewport, setViewport } = useReactFlow()
 
-    const [{ nodes, edges }, setLayout] = useState(
-        getLayoutedElements(courseNodeData.nodes, courseNodeData.edges)
-    )
+    const [{ nodes, edges }, setLayout] = useState<CourseNodeData>({
+        nodes: [],
+        edges: [],
+    })
 
     useEffect(() => {
-        const elements = getLayoutedElements(
-            courseNodeData.nodes,
-            courseNodeData.edges
-        )
-        setLayout(elements)
+        const layoutNodes = async () => {
+            const layoutedNodes = await getLayoutedNodes(
+                courseNodeData.nodes,
+                courseNodeData.edges
+            )
 
-        const fitView = async () => {
-            await reactFlow.fitView({ duration: 750 })
+            setLayout({ nodes: layoutedNodes, edges: courseNodeData.edges })
         }
 
+        void layoutNodes().then(async () =>
+            setTimeout(() => {
+                void fitView({ duration: 750 })
+            }, 10)
+        )
+
         // Only move the view if the number of nodes has changed
-        setTimeout(() => {
-            void fitView()
-        }, 50)
-    }, [courseNodeData, reactFlow, isMobile])
+    }, [fitView, courseNodeData, setViewport, getViewport])
 
     return (
         <ReactFlow

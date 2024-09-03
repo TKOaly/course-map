f798f5ed50fb56c1e3c519a9f7a17d76d6590b2f
Eetu Raunio <eetu.raunio@helsinki.fi>
2024-09-04 02:08:07 +0300

    Add boundary to prevent panning to infinity
    Memoizes a translate extent based on nodes' locations and uses it to restrict user pannig

--- app/components/flow/CourseFlow.tsx
+++ app/components/flow/CourseFlow.tsx
@@ -7,7 +7,7 @@ import { useSetAtom } from 'jotai'
 import { CourseNode } from './CourseNode'
 
 import '@xyflow/react/dist/style.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { CourseEdge } from './CourseEdge'
 import { type CourseNodeData } from './CourseLoader'
 
@@ -16,6 +16,7 @@ import ELK from 'elkjs'
 
 const nodeWidth = 350
 const nodeHeight = 80
+const edgePadding = 1000
 
 const elk = new ELK()
 
@@ -98,6 +99,40 @@ export const CourseFlow = ({
         edges: [],
     })
 
+    /**
+     * Calculate the translate extent based on the nodes' positions and sizes
+     * This is used to prevent the user from panning outside the graph
+     */
+    const translateExtent: [[number, number], [number, number]] =
+        useMemo(() => {
+            let [minX, maxX, minY, maxY] = [
+                Infinity,
+                -Infinity,
+                Infinity,
+                -Infinity,
+            ]
+
+            nodes.forEach((node) => {
+                minX = Math.min(minX, node.position.x)
+                maxX = Math.max(
+                    maxX,
+                    node.position.x +
+                        (node.width ?? node.initialWidth ?? nodeWidth)
+                )
+                minY = Math.min(minY, node.position.y)
+                maxY = Math.max(
+                    maxY,
+                    node.position.y +
+                        (node.height ?? node.initialHeight ?? nodeHeight)
+                )
+            })
+
+            return [
+                [minX - edgePadding, minY - edgePadding],
+                [maxX + edgePadding, maxY + edgePadding],
+            ]
+        }, [nodes])
+
     useEffect(() => {
         const layoutNodes = async () => {
             const layoutedNodes = await getLayoutedNodes(
@@ -141,6 +176,7 @@ export const CourseFlow = ({
             zoomOnPinch
             fitView
             attributionPosition={isMobile ? 'top-right' : 'bottom-left'}
+            translateExtent={translateExtent}
         >
             <Background color="#777" />
         </ReactFlow>
